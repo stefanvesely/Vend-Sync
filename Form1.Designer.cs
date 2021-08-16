8512a3160509abf864c447bff5bcfd85c4b1e7b9
stefanvesely <baws.steff@gmail.com>
2021-08-16 21:44:30 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace Vend_Sync
+{
+    partial class frmSyncServer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.brnAddNew = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnSendData = new System.Windows.Forms.Button();
+            this.btnRetrieveData = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // brnAddNew
+            // 
+            this.brnAddNew.Location = new System.Drawing.Point(250, 10);
+            this.brnAddNew.Name = "brnAddNew";
+            this.brnAddNew.Size = new System.Drawing.Size(75, 23);
+            this.brnAddNew.TabIndex = 0;
+            this.brnAddNew.Text = "Add New";
+            this.brnAddNew.UseVisualStyleBackColor = true;
+            this.brnAddNew.Click += new System.EventHandler(this.brnAddNew_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(232, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // btnSendData
+            // 
+            this.btnSendData.Location = new System.Drawing.Point(12, 126);
+            this.btnSendData.Name = "btnSendData";
+            this.btnSendData.Size = new System.Drawing.Size(202, 26);
+            this.btnSendData.TabIndex = 2;
+            this.btnSendData.Text = "Send";
+            this.btnSendData.UseVisualStyleBackColor = true;
+            this.btnSendData.Click += new System.EventHandler(this.btnSendData_Click);
+            // 
+            // btnRetrieveData
+            // 
+            this.btnRetrieveData.Location = new System.Drawing.Point(12, 95);
+            this.btnRetrieveData.Name = "btnRetrieveData";
+            this.btnRetrieveData.Size = new System.Drawing.Size(202, 25);
+            this.btnRetrieveData.TabIndex = 3;
+            this.btnRetrieveData.Text = "Get";
+            this.btnRetrieveData.UseVisualStyleBackColor = true;
+            this.btnRetrieveData.Click += new System.EventHandler(this.btnRetrieveData_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(220, 126);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(180, 25);
+            this.btnRefresh.TabIndex = 4;
+            this.btnRefresh.Text = "Get All Tables";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // frmSyncServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 210);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnRetrieveData);
+            this.Controls.Add(this.btnSendData);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.brnAddNew);
+            this.Name = "frmSyncServer";
+            this.Text = "Sync";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button brnAddNew;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnSendData;
+        private System.Windows.Forms.Button btnRetrieveData;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Vend_Sync
+{
+    public partial class frmSyncServer : Form
+    {
+        public string sConstringServer = "";
+        public string sConstringLocal = "";
+        public string sGetTables = @"SELECT t.name as TableName, SCHEMA_NAME(t.schema_id) As SchemaName,I.rows as RecordCount from sysindexes i INNER JOIN sys.tables t on i.id=t.object_id";
+
+        public frmSyncServer()
+        {
+            InitializeComponent();
+        }
+
+        private void brnAddNew_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnSendData_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnRetrieveData_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void RetrieveRemoteData()
+        {
+        }
+
+        private void RetrieveLocalData()
+        {
+        }
+
+        private void TransferFromRemote()
+        {
+        }
+
+        private void TransferToRemote()
+        {
+        }
+
+        private List<int> ListIndexsOfDiscrepencies(DataTable dtSourceTable, DataTable dtDestinationTable)
+        {
+            List<int> lstIndexList = new List<int>();
+            for (int iRow = 0; iRow < dtSourceTable.Rows.Count; iRow++)
+            {
+                if (iRow > dtDestinationTable.Rows.Count - 1)
+                {
+                    lstIndexList.Add(iRow);
+                }
+                else
+                {
+                    for (int iCol = 0; iCol < dtSourceTable.Columns.Count; iCol++)
+                    {
+                        if (dtSourceTable.Rows[iRow][iCol] != dtDestinationTable.Rows[iRow][iCol])
+                        {
+                            if (!lstIndexList.Contains(iRow))
+                            {
+                                lstIndexList.Add(iRow);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return lstIndexList;
+        }
+
+        private List<string> GetTableNames(string sGetConnString)
+        {
+            DataTable dtTableDataTable = new DataTable();
+            SqlConnection sqlConnection = new SqlConnection(sGetConnString);
+            sqlConnection.Open();
+            SqlDataAdapter sqlOilAdapter = new SqlDataAdapter(sGetTables, sqlConnection);
+            sqlOilAdapter.Fill(dtTableDataTable);
+            sqlConnection.Close();
+            List<string> lstTableNames = new List<string>();
+            foreach (DataRow drTable in dtTableDataTable.Rows)
+            {
+                string sTableName = drTable["TableName"].ToString();
+                if (!lstTableNames.Contains(sTableName))
+                {
+                    lstTableNames.Add(sTableName);
+                }
+            }
+            return lstTableNames;
+        }
+
+        private DataTable GetSingleTable(string sTableName, string sGetConnString)
+        {
+            DataTable dtTabletoReturn = new DataTable();
+            string sCommand = "SELECT * FROM " + sTableName;
+            SqlConnection sqlConnection = new SqlConnection(sGetConnString);
+            sqlConnection.Open();
+            SqlDataAdapter sqlOilAdapter = new SqlDataAdapter(sCommand, sqlConnection);
+            sqlOilAdapter.Fill(dtTabletoReturn);
+            sqlConnection.Close();
+            return dtTabletoReturn;
+        }
+
+        private bool SuperBulkUploadTable(string sDestinationTable, DataTable dtTableToUpload, string sConnString) // Only use when ulpoading a table with the same columns as dest table
+        {
+            bool isUploaded = true;
+            try
+            {
+                SqlConnection sqlConn = new SqlConnection(sConnString);
+                SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(sqlConn);
+                sqlBulkCopy.DestinationTableName = sDestinationTable;
+                foreach (DataColumn dcUploadColumn in dtTableToUpload.Columns)
+                {
+                    sqlBulkCopy.ColumnMappings.Add(dcUploadColumn.ColumnName, dcUploadColumn.ColumnName);
+                }
+                sqlConn.Open();
+                sqlBulkCopy.WriteToServer(dtTableToUpload);
+                sqlConn.Close();
+                isUploaded = true;
+            }
+            catch
+            {
+                isUploaded = false;
+            }
+            return isUploaded;
+        }
+
+        private void CleanTable(string sTableName, string sConnString)
+        {
+            string sQuery = @"TRUNCATE TABLE " + sTableName;
+            SqlConnection sqlCon = new SqlConnection(sConnString);
+            sqlCon.Open();
+            SqlCommand cmd = new SqlCommand(sQuery, sqlCon);
+            cmd.ExecuteNonQuery();
+            sqlCon.Close();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            List<string> lstTableNames = GetTableNames(sConstringServer);
+            foreach (string sTableName in lstTableNames)
+            {
+                if (sTableName.Contains("Vending"))
+                {
+                    DataTable dtTableToUpload = GetSingleTable(sTableName, sConstringServer);
+                    CleanTable(sTableName, sConstringLocal);
+                    SuperBulkUploadTable(sTableName, dtTableToUpload, sConstringLocal);
+                }
+            }
+        }
+    }
+}
